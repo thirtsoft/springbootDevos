f5d46dd833214741f992933623a3bab6e01a9831
thirtsoft <thirtsoft@gmail.com>
2021-08-16 16:48:38 +0000

    first commit

---
+++ src/main/java/com/devos/SpringbootDevosApplication.java
@@ -0,0 +1,13 @@
+package com.devos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDevosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devos/SpringbootDevosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDevosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
